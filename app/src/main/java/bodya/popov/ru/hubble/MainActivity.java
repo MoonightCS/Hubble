c8ee67b1068caab4f6873567d72af8bebc50280c
Popov Bogdan <Popov.B.V@sberbank.ru>
2017-11-24 17:21:01 +0300

    initial commit

---
+++ app/src/main/java/bodya/popov/ru/hubble/MainActivity.java
@@ -0,0 +1,13 @@
+package bodya.popov.ru.hubble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
